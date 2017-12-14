945fe1f84100ea0333ff5445f6550ed683f02e34
Charles Barto <bartoc@umich.edu>
2017-12-14 01:36:37 -0500

    more packages

--- init.el
+++ init.el
@@ -16,8 +16,14 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
+
+;; tell use package to always just install packages
+;; this makes it like Vim's managers (plugged and friends)
 (setq use-package-always-ensure t)
+
 (use-package no-littering)
+(use-package use-package-chords
+  :config (key-chord-mode 1))
 (use-package evil
   :config (evil-mode 1))
 
@@ -25,9 +31,11 @@
   :bind ("M-x" . helm-M-x)
   :config (helm-mode 1))
 
+(use-package helm-projectile)
 (use-package helm-descbinds
   :config (helm-descbinds-mode))
-
+(use-package which-key
+  :config (which-key-mode))
 (use-package company
   :config
   (global-company-mode)
@@ -37,7 +45,16 @@
 	 ("S-SPC" . helm-company)
 	 :map company-active-map
 	 ("S-SPC" . helm-company)))
-
+(use-package flycheck)
+(use-package meson-mode)
+(use-package irony)
+(use-package nim-mode
+  :hook (nim-mode-hook . nimsuggest-mode))
+(use-package paradox)
+(use-package esup)
+(use-package projectile)
+(use-package treemacs
+  :chords ("\\\\" . treemacs-toggle))
 (use-package dashboard
   :config (dashboard-setup-startup-hook))
 (use-package all-the-icons)
