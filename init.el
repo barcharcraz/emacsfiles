f53bd1ce3c6fd56e76ac5b88c69224deac0e7493
Charles Barto <bartoc@umich.edu>
2018-04-06 01:49:29 -0400

    thigns work now

--- init.el
+++ init.el
@@ -8,9 +8,15 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
 (package-initialize)
 
 (require 'org-install)
 (org-babel-load-file (expand-file-name "start.org" user-emacs-directory))
-
+(provide 'init)
 ;;; init.el ends here
