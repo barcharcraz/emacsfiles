973086739df25a1f393773ec3dc5a4513ce0f7f6
Charles Barto <bartoc@umich.edu>
2018-04-26 10:59:24 -0400

    fix custom file

--- init.el
+++ init.el
@@ -3,18 +3,6 @@
 ;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
 ;; embedded in literate Org-mode files.
 
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-
 (require 'org-install)
 (org-babel-load-file (expand-file-name "start.org" user-emacs-directory))
 (provide 'init)
